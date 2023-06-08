0a44feb0e5229ccd209e3cfffe8f22b6a667e7d8
Danila939 <100614360+Danila939@users.noreply.github.com>
2023-06-08 21:36:17 +0300

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,8 +1,8 @@
 def dfs(graph, start, len, visited=None):
     try:
         if visited is None:  # проверка посещена ли вершина
-            visited = set()  #новый коммент
-        visited.add(start)   #новый коммент
+            visited = set()  #new comment
+        visited.add(start)   #new comment 
     except Exception as e:
         print ("Node is not valid" + start)
         raise e
